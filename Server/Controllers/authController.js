e6051c6fa663ac96ff7561395a9e6b101dd632c2
Rafiullah Ahmadi <120250017+Ahmadi1798@users.noreply.github.com>
2025-06-26 15:11:54 +0430

    Update authController.js

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -1,5 +1,5 @@
 import admin from '../utils/firebaseAdmin.js';
-import { User } from '../models/userModel.js';
+import { User } from '../Models/userModel.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
