cf5d0cc89b26fa4ace8f700d55c411c83917c763
Rafiullah Ahmadi <120250017+Ahmadi1798@users.noreply.github.com>
2025-06-26 15:01:04 +0430

    Update userModel.js

--- Server/Models/userModel.js
+++ Server/Models/userModel.js
@@ -27,6 +27,5 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-export const User = mongoose.model('User', userSchema);
-
-('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
+const User = mongoose.model('User', userSchema);
+export default User;
